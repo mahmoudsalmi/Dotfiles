49d7b58854f107e939803cc31b7308c784a905a3
Mahmoud SALMI <mahmoud.salmi@gmail.com>
2022-01-15 01:28:20 +0100

    arch config

--- zsh/.config/nnn/init_nnn.zsh
+++ zsh/.config/nnn/init_nnn.zsh
@@ -1,16 +1,16 @@
-export NNN_SRC=$MS_SL_REPO/nnn
 export NNN_CFG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/nnn
 
 export NNN_TMPFILE="$NNN_CFG_DIR/.lastd"
 export NNN_SEL="$NNN_CFG_DIR/.sel"
 export NNN_FIFO="$NNN_CFG_DIR/nnn.fifo"
-export NNN_OPTS="ecGiHU"
+# export NNN_OPTS="ecGiHU"
+export NNN_OPTS="eciHU"
 
 n() {
   if [ -n "${NNNLVL}" ] && [ "${NNNLVL:-0}" -ge 1 ]; then
     echo "nnn is already running"
-  elif [[ -x "$NNN_SRC/nnn" ]]; then
-    "$NNN_SRC/nnn" "$@"
+  else
+    nnn "$@"
 
     if [[ -f "$NNN_TMPFILE" ]]; then
       . "$NNN_TMPFILE"

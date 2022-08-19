b5685e998888057c65d2e0177265c4c9915708a7
Mahmoud SALMI <mahmoud.salmi@msweb.net>
2022-08-19 12:37:53 +0200

    [programs] add arch-arm

---
+++ programs/arch-arm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# TODO: install paru
+
+# sed -e "s/#.*$//" -e "s/\s*$//"  -e "/^\s*$/d" ./arch/pkglist.txt | xargs -I {} sh -c "sudo pacman -S --confirm --needed {}"
+sudo pacman -S --needed $(sed -e "s/#.*$//" -e "s/\s*$//"  -e "/^\s*$/d" ./arch-arm/pkglist.txt | tr '\n' ' ')
+paru -S --needed $(sed -e "s/#.*$//" -e "/^\s*$/d" -e "/^\s*$/d" ./arch-arm/pkglist-aur.txt | tr '\n' ' ')
+

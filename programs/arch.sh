f5d23d9a70d7a252d184201ba9890a381693ad0f
Mahmoud SALMI <mahmoud.salmi@msweb.net>
2022-10-16 21:00:49 +0200

    [arch] exit when pacman packlist failed

--- programs/arch.sh
+++ programs/arch.sh
@@ -3,6 +3,6 @@
 # TODO: install paru
 
 # sed -e "s/#.*$//" -e "s/\s*$//"  -e "/^\s*$/d" ./arch/pkglist.txt | xargs -I {} sh -c "sudo pacman -S --confirm --needed {}"
-sudo pacman -S --needed $(sed -e "s/#.*$//" -e "s/\s*$//"  -e "/^\s*$/d" ./arch/pkglist.txt | tr '\n' ' ')
+sudo pacman -S --needed $(sed -e "s/#.*$//" -e "s/\s*$//"  -e "/^\s*$/d" ./arch/pkglist.txt | tr '\n' ' ') || exit 100
 paru -S --needed $(sed -e "s/#.*$//" -e "/^\s*$/d" -e "/^\s*$/d" ./arch/pkglist-aur.txt | tr '\n' ' ')
 

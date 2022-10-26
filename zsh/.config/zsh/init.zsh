735bf6d4e9c6fccb1bedea5c2141390ae12f26d3
Mahmoud SALMI <mahmoud.salmi@msweb.net>
2022-10-26 15:35:37 +0200

    [zsh] add group to exa

--- zsh/.config/zsh/init.zsh
+++ zsh/.config/zsh/init.zsh
@@ -231,10 +231,10 @@ alias vi=nvim
 alias vim=nvim
 
 alias ls='exa --color=always --group-directories-first' # my preferred listing
-alias la='exa -al --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -l -snew --color=always --group-directories-first' # tree listing
-alias l='exa -al -snew --color=always --group-directories-first'
+alias la='exa -al --color=always --group --group-directories-first'  # all files and dirs
+alias ll='exa -l --color=always --group --group-directories-first'  # long format
+alias lt='exa -l -snew --color=always --group --group-directories-first' # tree listing
+alias l='exa -al -snew --color=always --group --group-directories-first'
 
 alias wk="cd ~/_work/_wk"
 

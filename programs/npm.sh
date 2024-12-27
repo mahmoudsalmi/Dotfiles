d0d7a43bcbc6ef7540cecc24d97a787711f7bb47
Mahmoud SALMI <mahmoud@salmi.dev>
2024-12-27 22:16:26 +0100

    update npm.sh

--- programs/npm.sh
+++ programs/npm.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
 if command -v npm > /dev/null; then
-  npm i -g npm
-  npm i --location=global pnpm
+npm i -g npm
+npm i --location=global pnpm
 
-  pnpm i -g \
-    yarn \
-    pnpm \
-    degit \
-    eslint \
-		ls_emmet \
-		neovim \
-		prettier \
-		sass \
-		typescript-language-server \
-		typescript \
-    svelte-language-server
+pnpm i -g yarn pnpm degit eslint ls_emmet neovim prettier sass typescript-language-server typescript svelte-language-server
 fi
